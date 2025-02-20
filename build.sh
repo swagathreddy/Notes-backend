80ee2f1e2c07ef625ef7ad399e23037aedc43bd1
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-02-20 12:23:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 apt-get update
 apt-get install -y default-libmysqlclient-dev pkg-config build-essential python3-dev
 
-# Set up virtual environment and install Python packages
-python -m venv /opt/venv
-source /opt/venv/bin/activate
+# Install Python packages
 pip install --upgrade pip
 pip install -r requirements.txt
 
